8ae1005375fbde1721a781c007022e9f891fb2eb
Marc-Antoine Bouchard M <marcantoine.bouchardm@gmail.com>
2014-11-05 18:49:40 -0500

    Initial commit

---
+++ src/recyclapp/RecyclApp.java
@@ -0,0 +1,19 @@
+package recyclapp;
+import javax.swing.JFrame;
+
+/**
+ * Application RecyclApp
+ * @author Marc-Antoine Bouchard M
+ */
+public class RecyclApp {
+
+    /**
+     * Main
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame fenetre = new JFrame();
+        fenetre.setVisible(true);
+    }
+    
+}
