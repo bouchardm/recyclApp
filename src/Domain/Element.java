57954ea2419899aa3729190a6ebc9b770cc801a1
Danny <danny.lebel.1@ulaval.ca>
2014-11-12 12:21:49 -0500

    suppression des attributs de Element qui n'ont pas de justification

--- src/Domain/Element.java
+++ src/Domain/Element.java
@@ -3,12 +3,7 @@ package Domain;
 
 import java.awt.geom.Point2D;
 
-public abstract class Element extends MatterBasket {
-	public SortStation _sortStation;
-	public EntryPoint _entryPoint;
-	public ExitPoint _exitPoint;
-	public Conveyor _conveyor;
-	public Junction _junction;
-
+public abstract class Element
+{
 	public abstract boolean include(Point2D.Float point);
 }
