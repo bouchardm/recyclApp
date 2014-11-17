fffb6b725e9f4e91fcc00510e07c3894fd39bc8d
simon rodier <simon@mopigeons.com>
2014-11-17 14:35:49 -0500

    implémentation du sortMatrix et tests unitaires légères corrections au matterlist ajout d'une liste d'outlet au sortstation début de transmatrix

--- src/Domain/SortMatrix.java
+++ src/Domain/SortMatrix.java
@@ -1,15 +1,177 @@
 package Domain;
 
 
-import Domain.SortStation;
-import java.util.Dictionary;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class SortMatrix {
-	private Integer _iD;
-	private Dictionary _sortMatrix;
-        //pourquoi est-ce que SortMatrix a deux sortstation?
-	public SortStation _sortStation;
-	public SortStation _sortStation2;
+    /* 
+    _sortMatrix: Le premier Integer est le MatterID. Ensuite, on a une liste
+    qui contient la matière qui doit se retrouver à chaque sortie, en ordre
+    de l'index de la liste. L'index 0 doit être la sortie 1, l'index 1 la sortie 2, etc.
+    */
+    private HashMap<Integer, ArrayList<Float>> _sortMatrix;
+
+    public SortMatrix() {
+        _sortMatrix = new HashMap<>();
+    }
+    
+    //constructeur : recoit matterList et outletList en paramètre
+    //crée une matrice qui met toutes les matières à la première sortie dans la liste
+    public SortMatrix(MatterList aMatterList, ArrayList<Outlet> outletList) {
+        _sortMatrix = new HashMap<>();
+        for(int i = 0; i<aMatterList.GetCount(); i++) {
+            ArrayList<Float> matrixOutlets = new ArrayList<>();
+            for (int j = 0; j<outletList.size(); j++) {
+                if(j==0) {
+                    Float aQuantity;
+                    aQuantity = new Float(1);
+                    matrixOutlets.add(aQuantity);
+                }
+                else {
+                    Float aQuantity;
+                    aQuantity = new Float(0);
+                    matrixOutlets.add(aQuantity);
+                }
+            }
+            _sortMatrix.put(aMatterList.getMatterID(i), matrixOutlets);   
+        }
+    }
+
+    /*addMatterToSortMatrix(matterID, matterPerOutlet): pour chaque matière qu'on ajoute à la matrice, on ajoute: le numéro ID de la matière
+    ainsi qu'une liste contenant l'index de la sortie dans la liste et la quantité qui doit se retourner
+    à la sortie.*/
+    //precondition : matterId est valide
+    public void addMatterToSortMatrix(Integer matterID, ArrayList<Float> matterPerOutlet) {
+        _sortMatrix.put(matterID, matterPerOutlet);
+    }
+    
+    /*addToSortMatrix: pour chaque matière qu'on ajoute à la matrice, on ajoute: le numéro ID de la matière
+                       On met toute la quantité à sortir dans la première outlet.  */
+    //precondition : matterId est valide
+    public void addMatterToSortMatrix(Integer matterID) {
+        ArrayList<Float> matterPerOutlet = new ArrayList<>();
+        Iterator<Map.Entry<Integer, ArrayList<Float>>> smIter = _sortMatrix.entrySet().iterator();
+        if(!smIter.hasNext()) {
+            matterPerOutlet.add(new Float(1));
+            _sortMatrix.put(matterID, matterPerOutlet);
+        }
+        else {
+            Map.Entry<Integer, ArrayList<Float>> currentEntry = smIter.next();
+            int outletListSize = currentEntry.getValue().size();
+            for (int i = 0; i < outletListSize; i++) {
+                if(i==0) {
+                    matterPerOutlet.add(new Float(1));
+                }
+                else {
+                    matterPerOutlet.add(new Float(0));
+                }
+            }
+            _sortMatrix.put(matterID, matterPerOutlet);
+        }
+    }
+
+    //enleve une matiere par son numéro ID
+    public void removeMatterFromMatrix(Integer matterID) {
+        if((!_sortMatrix.containsKey(matterID)) || _sortMatrix.isEmpty()) {
+            throw new IllegalArgumentException("Effacement impossible : la matière n'est pas dans la matrice.");
+        }
+         _sortMatrix.remove(matterID);
+    }
+    
+
+    //precondition : l'index passé en paramètre correspond au numéro de la sortie-1
+                            //ex: Sortie1 = index 0 ... Sortie2 = index1, etc.
+    //precondition : on ne peut pas effacer une sortie dans une matrice qui en a déjà pas
+    //PAS TESTÉE
+    public void removeOutletFromMatrix(Integer outletListIndex) {
+        Iterator<Map.Entry<Integer, ArrayList<Float>>> smIter = _sortMatrix.entrySet().iterator();
+        if(outletListIndex==0) {        //si oui
+            //pour toutes les matieres, 
+                //prendre les quantités de la première sortie et les mettres dans la deuxième
+            while(smIter.hasNext()) {
+                Map.Entry<Integer, ArrayList<Float>> currentEntry = smIter.next();
+                Integer currentMatterId = currentEntry.getKey();        //le ID de la matière courante
+                ArrayList<Float> currentOutletQuantities = currentEntry.getValue();  //la liste associée à cette matière
+                if (currentOutletQuantities.size()==1) {
+                    currentOutletQuantities.remove(0);
+                }
+                else {
+                    float qtyAtFirstOutlet = currentOutletQuantities.get(0);
+                    float qtyAtSecondOutlet = currentOutletQuantities.get(1);
+                    float newQtyAtFirstOutlet = qtyAtFirstOutlet + qtyAtSecondOutlet;
+                    currentOutletQuantities.remove(0);
+                    currentOutletQuantities.remove(1);
+                    //la nouvelle premiere entrée est la sortie 3 originale... mais elle doit devenir la deuxième
+                    currentOutletQuantities.add(0, newQtyAtFirstOutlet);  
+                }
+            }
+        }
+        else {                          //si non
+            //prendre les quantités de la sortie à effacer et les mettres dans la première
+            while(smIter.hasNext()) {
+                Map.Entry<Integer, ArrayList<Float>> currentEntry = smIter.next();
+                Integer currentMatterId = currentEntry.getKey();
+                ArrayList<Float> currentOutletQuantities = currentEntry.getValue();
+                if(currentOutletQuantities.size()==1) {
+                    currentOutletQuantities.remove(0);
+                }
+                else {
+                    float qtyAtFirstOutlet = currentOutletQuantities.get(0);
+                    float qtyAtDeletedOutlet = currentOutletQuantities.get(outletListIndex);
+                    float newQtyAtFirstOutlet = qtyAtFirstOutlet + qtyAtDeletedOutlet;
+                    currentOutletQuantities.remove(0);
+                    currentOutletQuantities.add(0, newQtyAtFirstOutlet);
+                    currentOutletQuantities.remove(outletListIndex.intValue());   
+                }
+            }
+        }
+
+    }
+    
+    //enleve tout les matières de la sortMatrix (reste une matrice vide)
+    public void removeAllMatterFromSortMatrix() {        
+        Iterator<Map.Entry<Integer, ArrayList<Float>>> smIter = _sortMatrix.entrySet().iterator();
+        while(smIter.hasNext()) {
+            Map.Entry<Integer, ArrayList<Float>> currentEntry = smIter.next();
+            smIter.remove();
+        }
+    }
+    
+    //ajoute un outlet à la fin des tableau de quantité. les quantités sont à 0.
+    public void addOutletToSortMatrix() {
+        Iterator<Map.Entry<Integer, ArrayList<Float>>> smIter = _sortMatrix.entrySet().iterator();
+        while(smIter.hasNext()) {
+            Map.Entry<Integer, ArrayList<Float>> currentEntry = smIter.next();
+            _sortMatrix.get(currentEntry.getKey()).add(new Float(0));
+        }
+    }
+    
+    //retourne une copie profonde de _sortMatrix
+    public HashMap<Integer, ArrayList<Float>> getSortMatrix()
+    {
+        HashMap<Integer, ArrayList<Float>> copiedSortMatrix = new HashMap<>();
+        for(Integer key : _sortMatrix.keySet()) {
+            ArrayList<Float> copiedInnerList = new ArrayList<>();
+            for(int j = 0; j<_sortMatrix.get(key).size(); j++) {
+                Float quantityAtExit = _sortMatrix.get(key).get(j);
+                copiedInnerList.add(quantityAtExit);
+            }
+            copiedSortMatrix.put(key, copiedInnerList);
+        }
+        return copiedSortMatrix;
+    }
+    
+    
+    public int getMatterCount() {
+        return _sortMatrix.size();
+    }
+        
+        
         
         
 }
---
+++ test/Domain/SortMatrixTest.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Simon
+ */
+public class SortMatrixTest {
+    
+    public SortMatrixTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    //constructeur sans paramètres
+    @Test
+    public void SortMatrixConstructor_Success() {
+        SortMatrix sm = new SortMatrix();
+        assertTrue(sm.getMatterCount()==0);
+    }
+    
+    //constructeur avec paramètres (list matières et liste outlet)
+    @Test
+    public void SortMatrixConstructor_wParam_Success() {
+        Matter tm1 = new Matter("matter1", 1);
+        Matter tm2 = new Matter("matter2", 2);
+        MatterList tmlist = new MatterList();
+        tmlist.Add(tm1);
+        tmlist.Add(tm2);
+        SortStation st = new SortStation();
+        Outlet o1 = new Outlet(st);
+        Outlet o2 = new Outlet(st);
+        ArrayList<Outlet> outletList = new ArrayList<>();
+        outletList.add(o1);
+        outletList.add(o2);
+        SortMatrix sorter = new SortMatrix(tmlist, outletList);
+        //la matrice contient 2 éléments
+        assertTrue(sorter.getMatterCount()==2);
+        assertTrue(sorter.getSortMatrix().containsKey(1));
+        //la quantité à la première sortie est 1 (100%)
+        assertTrue(sorter.getSortMatrix().get(1).get(0)==1);
+        //la quantité à la deuxième sortie est 0 (0%)
+        assertTrue(sorter.getSortMatrix().get(1).get(1)==0);
+    }
+    
+    
+    
+    
+    //addMatterToSortMatrix (matterId et liste)
+    //public void addMatterToSortMatrix(Integer matterID, ArrayList<Float> matterPerOutlet)
+    @Test
+    public void addMatterToSortMatrixTest_withList_success() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        ArrayList<Float> mpo = new ArrayList<>();
+        Float q1;
+        q1 = new Float (0.25);
+        Float q2;
+        q2 =new Float(0.75);
+        mpo.add(q1);
+        mpo.add(q2);
+        sm.addMatterToSortMatrix(m1.getID(), mpo);
+        assertTrue(sm.getMatterCount()==1);
+        assertTrue(sm.getSortMatrix().containsKey(1));
+        assertFalse(sm.getSortMatrix().containsKey(22));  
+        assertTrue(sm.getSortMatrix().get(1).get(1).floatValue()==0.75);
+    }
+    
+    //addMatterToSortMatrix (matterId)
+    @Test
+    public void addMatterToSortMatrix_noList_success() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        Matter m2 = new Matter("m2",2);
+        sm.addMatterToSortMatrix(m1.getID());
+        sm.addMatterToSortMatrix(m2.getID());
+        assertTrue(sm.getMatterCount()==2);
+        assertTrue(sm.getSortMatrix().containsKey(m1.getID()));
+        assertTrue(sm.getSortMatrix().get(m2.getID()).get(0)==1);
+    }
+    
+    //removeMatterFromMatrix
+    //cas: succes
+    @Test
+    public void removeMatterFromMatrixTest_success() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        sm.addMatterToSortMatrix(m1.getID());
+        assertTrue(sm.getMatterCount()==1);
+        sm.removeMatterFromMatrix(m1.getID());
+        assertTrue(sm.getMatterCount()==0);
+    }
+    //cas: la matière n'est pas dans la liste
+    @Test(expected = IllegalArgumentException.class)
+    public void removeMatterFromMatrixTest_fail_notInMatrix() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        Matter m2 = new Matter("m2",2);
+        sm.addMatterToSortMatrix(m2.getID());
+        sm.removeMatterFromMatrix(m1.getID());  
+    }
+    //cas: la liste est vide
+    @Test (expected = IllegalArgumentException.class)
+    public void removeMatterFromMatrixTest_fail_emptyMatrix() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        sm.removeMatterFromMatrix(m1.getID());
+    }
+    
+    //removeAllMatterFromSortMatrix
+    //cas: succes, avec matières dans la liste
+    @Test
+    public void removeAllMatterFromSortMatrixTest_success() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        Matter m2 = new Matter("m2",2);
+        sm.addMatterToSortMatrix(m1.getID());
+        sm.addMatterToSortMatrix(m2.getID());
+        sm.removeAllMatterFromSortMatrix();
+        assertTrue(sm.getMatterCount()==0);
+    }
+    //cas: succes, aucune matière dans la liste
+    @Test
+    public void removeAllMatterFromSortMatrixTest_success_emptyMatrix() {
+        SortMatrix sm = new SortMatrix();
+        sm.removeAllMatterFromSortMatrix();
+        assertTrue(sm.getMatterCount()==0);
+    }
+    
+    //getSortMatrix
+    @Test
+    public void getSortMatrixTest_deepCopy_success() {
+        SortMatrix sm = new SortMatrix();
+        Matter m1 = new Matter("m1",1);
+        Matter m2 = new Matter("m2",2);
+        sm.addMatterToSortMatrix(m1.getID());
+        sm.addMatterToSortMatrix(m2.getID());
+        HashMap<Integer, ArrayList<Float>> sm2 = sm.getSortMatrix();
+        assertTrue(sm2.size()==sm.getMatterCount());
+        sm2.remove(1);
+        //en changeant la copie, il y a maintenant différence entre les deux matrices
+        assertFalse(sm2.size()==sm.getMatterCount());
+    }
+    
+    
+    
+    //removeOutletFromMatrix
+    //cas : succes
+    @Test
+    public void removeOutletFromMatrix_success() {
+        //todo
+    }
+    //cas : hashmap vide
+    //todo
+    
+    
+    
+    
+    //addOutletToSortMatrix
+    //todo
+}
