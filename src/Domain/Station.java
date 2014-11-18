3bf37a9c148adf7a560487527e2e908c819a379d
keita-shimoda <keita.shimoda.1@gmail.com>
2014-11-17 20:42:25 -0500

    update

--- src/Domain/Station.java
+++ src/Domain/Station.java
@@ -7,32 +7,17 @@
 package Domain;
 
 import java.awt.Image;
-import java.util.ArrayList;
+
 
 
 public class Station extends RectangularNode
 {
     protected Image _img;
+    
     public void setExit(int nbExit) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    private Inlet _inlet;
-    private ArrayList<Outlet> _outlets;
- 
-//        _inlet = new Inlet(this);
-//        _outlets = new ArrayList<>();
-//        setExit(nbOutlet);
-        
+    }
 
     
-        @Override
-    public void setExit(int nbExit)
-    {
-        for (int i = 0; i < nbExit ; i++)
-        {
-            Outlet outlet = new Outlet(this);
-            _outlets.add(outlet);
-        }
-    }
-    }
     
 }
