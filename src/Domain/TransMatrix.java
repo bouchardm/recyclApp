79b3032f46fe150a3f6073a1b5edfea01186e356
simon rodier <simon@mopigeons.com>
2014-11-17 19:16:08 -0500

    iimplementé constructeurs transmatrix

--- src/Domain/TransMatrix.java
+++ src/Domain/TransMatrix.java
@@ -3,6 +3,9 @@ package Domain;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
 
 public class TransMatrix {
     //l'attribut transformMatrix : HashMap<MatterID, <Array de qté par matière, dans le même ordre que le matterList utilisé pour le construire>>
@@ -34,20 +37,66 @@ public class TransMatrix {
         }
     }
     
+    //ajoute une matière aux deux hashmaps sans transformation
+    //ex: si on ajoute la matière B, la matière B sera transformé à 100% en matière B
+    //toutes les autres matières seront transformé à 0% en matière B
     public void addMatterToTransMatrix(Integer matterID) {
-        //todo
+        //créer map pour la nouvelle matière        
+        HashMap<Integer, Float> transformQty = new HashMap<>();
+        //si la matrice est vide, on ne fait qu'ajouter la matière et elle se transforme en elle même
+        if(!_transformMatrix.isEmpty()) {
+            Iterator<Map.Entry<Integer, HashMap<Integer, Float>>> tmIter = _transformMatrix.entrySet().iterator();
+            while (tmIter.hasNext()) {
+                Map.Entry<Integer, HashMap<Integer, Float>> currentEntry = tmIter.next();
+                Integer currentMatterID = currentEntry.getKey();
+                //on ajoute 0 pour toutes les matter ID
+                transformQty.put(currentMatterID, new Float(0));
+            }
+        }
+        //on mets transformation à 100% de la matière à elle même
+        transformQty.put(matterID, new Float(1));
+        _transformMatrix.put(matterID, transformQty); 
     }
     
-    public void addMatterToTransMatrix(Integer matterID, ArrayList<Float> matterQuantities) {
-        //todo
+    //ajoute la nouvelle matière et les quantités dans lequel elle se transforme dans le transformMatrix
+    //ajoute une entrée pour toutes les autres matières indiquant qu'ils se transforment à 0%
+    //en cette nouvelle matière
+    //le HashMap en entrée (transformQuantities) doit contenir une valeur de transformation pour
+    //toutes les matières du matterList incluant la matière elle même
+    public void addMatterToTransMatrix(Integer matterID, HashMap<Integer, Float> transformQuantities) {
+        //boucle dans le hashmap. pour chaque clé, on ajoute au hashMap interne l'entry (matterID, 0).
+        Iterator<Map.Entry<Integer, HashMap<Integer, Float>>> tmIter = _transformMatrix.entrySet().iterator();
+        while(tmIter.hasNext()) {
+            HashMap<Integer, Float> newTransformQuantities = new HashMap<>();
+            Map.Entry<Integer, HashMap<Integer, Float>> currentEntry = tmIter.next();
+            Integer currentID = currentEntry.getKey();
+            //on va chercher les valeurs courantes du hashmap interne pour le recopier dans un nouveau
+            //hashmap avec les nouvelles valeurs
+            HashMap<Integer, Float> matterTransformQuantities = currentEntry.getValue();
+            Iterator<Map.Entry<Integer, Float>> innerIter = matterTransformQuantities.entrySet().iterator();
+            while(innerIter.hasNext()) {
+                Map.Entry<Integer, Float> currentInnerEntry = innerIter.next();
+                Integer innerKey = currentInnerEntry.getKey();
+                Float innerQty = currentInnerEntry.getValue();
+                newTransformQuantities.put(innerKey, innerQty);
+                //effacer l'entry courante (du inner hashmap)
+                //!!!!!!!!!ICI!!!!!!!
+            }
+            //rajouter les nouvelles quantités pour chaque matière à _transformMatrix
+        }
+        //ajout au transformMatrix : (matterID, transformQuantities)
+        
     }
     
     public int getMatterCount() {
-        //todo
+        return _transformMatrix.size();
     }
     
-    public TransMatrix getTransMatrix() {
+    //doit retourner une copie profonde de transMatrix
+    public HashMap<Integer, HashMap<Integer, Float>> getTransMatrix() {
+        HashMap<Integer, HashMap<Integer, Float>> copiedMatrix = new HashMap<>();
         //todo
+        return copiedMatrix;
     }
     
     public void removeAllMatterFromTransMatrix() {
