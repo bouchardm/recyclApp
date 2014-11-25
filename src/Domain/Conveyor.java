0b3e057c9e60394ccc440d9eb8f1cd0368c38f55
Marc-Antoine Bouchard M <marcantoine.bouchardm@gmail.com>
2014-11-24 19:16:51 -0500

    Correction des indentations

--- src/Domain/Conveyor.java
+++ src/Domain/Conveyor.java
@@ -7,100 +7,100 @@ import java.awt.geom.Point2D;
 
 public class Conveyor extends Element
 {
-        private static final float DEFAULTCAPACITY = 10; // 10 kg/h
-	private Outlet _startPoint;
-	private Inlet _endPoint;
-	private Object _iD;
-	private MatterBasket _matterBasket;
-        private Float _speedMax;
-        private Line2D.Float _line;
-        
-        private final static float _WIDTH = 0.5f;
-        
-        public Conveyor(Outlet startPoint, Inlet endPoint)
+    private static final float DEFAULTCAPACITY = 10; // 10 kg/h
+    private Outlet _startPoint;
+    private Inlet _endPoint;
+    private Object _iD;
+    private MatterBasket _matterBasket;
+    private Float _speedMax;
+    private Line2D.Float _line;
+
+    private final static float _WIDTH = 0.5f;
+
+    public Conveyor(Outlet startPoint, Inlet endPoint)
+    {
+        if (startPoint == null || endPoint == null)
         {
-            if (startPoint == null || endPoint == null)
-            {
-                throw new IllegalArgumentException("startPoint and endPoint cannot be null");
-            }
-            if (startPoint.getNode()==endPoint.getNode())
-            {
-                throw new IllegalArgumentException("startPoint and endPoint cannot be connected to same Node.");
-            }
-            _line = new Line2D.Float();
-            this._startPoint = startPoint;
-            this._endPoint = endPoint;           
-            _startPoint.setConveyor(this);
-            
-            if (endPoint.getNode().getClass() != Junction.class)
-                _endPoint.setConveyor(this);
-            else
-                _endPoint.addConveyor(this);
-           
-            _speedMax = DEFAULTCAPACITY;
-            
-            updatePoints();
+            throw new IllegalArgumentException("startPoint and endPoint cannot be null");
         }
-        
-        
-        @Override
-        public boolean include(Point2D.Float point)
+        if (startPoint.getNode()==endPoint.getNode())
         {
-            updatePoints();
-            return ((float)_line.ptSegDist(point)) <= _WIDTH/2;
+            throw new IllegalArgumentException("startPoint and endPoint cannot be connected to same Node.");
         }
-        
-        public void updatePoints()
-        {
-            Point2D.Float p1 = _startPoint.getPosition();
-            Point2D.Float p2 = _endPoint.getPosition();
-            _line.setLine(p1.x, p1.y, p2.x, p2.y);
-        }
-        
-        public Outlet getStartOutlet() {
-            return this._startPoint;
-        }
-        
-        public Inlet getEndInlet() {
-            return this._endPoint;
-        }
-        
-        
-        public Node getStartNode()
-        {
-            return _startPoint.getNode();
-        }
-        
-        public Node getEndNode()
-        {
-            return _endPoint.getNode();
-        }
-        
-        public MatterBasket getMatterBasket()
-        {
-            return _startPoint.getMatterBasket();
-        }
-        
-        public Point2D.Float getPoint1()
-        {
-            updatePoints();
-            return (Point2D.Float)_line.getP1();
-        }
-        
-        
-        public Point2D.Float getPoint2()
-        {
-            updatePoints();
-            return (Point2D.Float)_line.getP2();
-        }
-        
-        public void removeConveyor() {
-           _startPoint.removeConveyor();
-           _endPoint.removeConveyor();
-            _startPoint = null;
-            
-            _endPoint = null;
-        }
+        _line = new Line2D.Float();
+        this._startPoint = startPoint;
+        this._endPoint = endPoint;           
+        _startPoint.setConveyor(this);
+
+        if (endPoint.getNode().getClass() != Junction.class)
+            _endPoint.setConveyor(this);
+        else
+            _endPoint.addConveyor(this);
+
+        _speedMax = DEFAULTCAPACITY;
+
+        updatePoints();
+    }
+
+
+    @Override
+    public boolean include(Point2D.Float point)
+    {
+        updatePoints();
+        return ((float)_line.ptSegDist(point)) <= _WIDTH/2;
+    }
+
+    public void updatePoints()
+    {
+        Point2D.Float p1 = _startPoint.getPosition();
+        Point2D.Float p2 = _endPoint.getPosition();
+        _line.setLine(p1.x, p1.y, p2.x, p2.y);
+    }
+
+    public Outlet getStartOutlet() {
+        return this._startPoint;
+    }
+
+    public Inlet getEndInlet() {
+        return this._endPoint;
+    }
+
+
+    public Node getStartNode()
+    {
+        return _startPoint.getNode();
+    }
+
+    public Node getEndNode()
+    {
+        return _endPoint.getNode();
+    }
+
+    public MatterBasket getMatterBasket()
+    {
+        return _startPoint.getMatterBasket();
+    }
+
+    public Point2D.Float getPoint1()
+    {
+        updatePoints();
+        return (Point2D.Float)_line.getP1();
+    }
+
+
+    public Point2D.Float getPoint2()
+    {
+        updatePoints();
+        return (Point2D.Float)_line.getP2();
+    }
+
+    public void removeConveyor() {
+       _startPoint.removeConveyor();
+       _endPoint.removeConveyor();
+        _startPoint = null;
+
+        _endPoint = null;
+    }
 
     @Override
     public void setAttribute(String attribName, Object value) {
