b7c0335bda220325b36731c823e8a57dc31b4bf2
keita-shimoda <keita.shimoda.1@gmail.com>
2014-11-24 21:50:41 -0500

    Fix Multiple conveyor on exit point

--- src/Presentation/Swing/MainFrame.java
+++ src/Presentation/Swing/MainFrame.java
@@ -785,8 +785,8 @@ public class MainFrame extends javax.swing.JFrame {
                     viewport.setConnectingArrow(null);
                     _controller.setInlet();
 
-                    boolean isJunction = _controller.getInlet().getNode().getClass() == Junction.class
-                            || _controller.getInlet().getNode().getClass() == ExitPoint.class;
+                    boolean isJunction = _controller.getInlet().getNode().getClass() == Junction.class;
+                    // || _controller.getInlet().getNode().getClass() == ExitPoint.class;
                     if (!isJunction && !_controller.getInlet().IsFree()) {
                         viewport.setConnectingArrow(null);
                         JOptionPane.showMessageDialog(null, "L'entrée sélectionnée n'est pas libre", null, 0);
@@ -795,9 +795,7 @@ public class MainFrame extends javax.swing.JFrame {
                         return;
                     }
                     _controller.addConveyor();
-                }
-                else
-                {
+                } else {
                     JOptionPane.showMessageDialog(null, "Choisir une entrée", null, 0);
                 }
                 viewport.setCreationMode(Viewport.CREATION_MODES.NONE);
