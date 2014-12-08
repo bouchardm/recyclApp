453d0bd5c4a387461be525175901130e537ca225
Marc-Antoine Bouchard M <marcantoine.bouchardm@gmail.com>
2014-12-07 20:59:09 -0500

    Correction nom colonne matrice de tri

--- src/Presentation/Swing/OutletMatterFrame.java
+++ src/Presentation/Swing/OutletMatterFrame.java
@@ -59,7 +59,7 @@ public class OutletMatterFrame extends javax.swing.JFrame {
         
         exits[0] = "Quantité entrante";
         exits[1] = "Matière";
-        for (int i = 1; i < exits.length; i++) {
+        for (int i = 2; i < exits.length; i++) {
             exits[i] = "sortie " + i;   
         }
         
